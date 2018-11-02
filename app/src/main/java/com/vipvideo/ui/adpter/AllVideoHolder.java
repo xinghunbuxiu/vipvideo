29f188b49099653afcd724845bb398c07aca10fe
lixiaohong <lixiaohong@p2peye.com>
2018-11-02 19:35:56 +0800

    增加解析地址

---
+++ app/src/main/java/com/vipvideo/ui/adpter/AllVideoHolder.java
@@ -0,0 +1,42 @@
+package com.vipvideo.ui.adpter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.lixh.base.adapter.VBaseHolder;
+import com.vipvideo.R;
+import com.vipvideo.bean.AllVideoInfo.VideoListBean.VideosBean;
+import com.vipvideo.view.RoundImageView;
+
+import butterknife.Bind;
+
+/**
+ * Created by Moushao on 2017/8/30.
+ */
+
+public class AllVideoHolder extends VBaseHolder<VideosBean> {
+    @Bind(R.id.iv_video)
+    RoundImageView iv_image;
+    @Bind(R.id.tv_title)
+    TextView tv_title;
+
+    public AllVideoHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void setData(int ps, VideosBean data) {
+        super.setData(ps, data);
+        Glide.with(mContext).load(data.getImg_url()).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL).into(iv_image);
+        tv_title.setText(data.getTitle());
+    }
+
+    @Override
+    public void init() {
+        mView.setOnClickListener((v -> {
+            if (mListener != null) mListener.onItemClick(v, position, mData);
+        }));
+    }
+}
